a57ca2c62873b544ad1ad09af3518c6cba8ca80f
Lucas G. Reinert <lucasreinert96@gmail.com>
2024-02-08 17:45:30 -0300

    a festa do vue começou

---
+++ vueFolder/parking_lot_system/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <ParkIndex/>
+  </div>
+</template>
+
+<script>
+import ParkIndex from './components/ParkIndex.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ParkIndex
+  }
+}
+</script>
---
+++ vueFolder/parking_lot_system/src/components/ParkIndex.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="park-index">
+    <h1>Park Index</h1>
+    <!-- Conteúdo da tela inicial aqui -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ParkIndex',
+}
+</script>
+
+<style scoped>
+/* Estilos específicos do ParkIndex aqui */
+</style>
